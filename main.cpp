580c79207ba85e1c99758ec553275f6f0dae84ec
Anish-Bikram-Nembang <anish.bikram.nembang@gmail.com>
2026-09-01 17:57:55 +0545

    a moving hello world

--- main.cpp
+++ main.cpp
@@ -1,3 +1,25 @@
 #include <raylib.h>
 
-int main(void) { return 0; }
+int main(void) {
+  InitWindow(800, 450, "Khukuri");
+  SetTargetFPS(60);
+
+  Vector2 position{400, 255};
+
+  while (!WindowShouldClose()) {
+    if (IsKeyDown(KEY_RIGHT))
+      position.x += 4;
+    if (IsKeyDown(KEY_LEFT))
+      position.x -= 4;
+    if (IsKeyDown(KEY_UP))
+      position.y -= 4;
+    if (IsKeyDown(KEY_DOWN))
+      position.y += 4;
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawText("Hello, World!", position.x, position.y, 20, LIGHTGRAY);
+    EndDrawing();
+  }
+  CloseWindow();
+  return 0;
+}
