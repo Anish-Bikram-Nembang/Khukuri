9a2bab4ba25736c60f7b01980f0cfd7c9de62585
Anish-Bikram-Nembang <anish.bikram.nembang@gmail.com>
2026-09-01 21:39:07 +0545

    a jumping rectangle

--- main.cpp
+++ main.cpp
@@ -1,23 +1,48 @@
 #include <raylib.h>
 
 int main(void) {
-  InitWindow(800, 450, "Khukuri");
+  enum GameState { JUMPING, FALLING, ON_GROUND, OVER };
+  const int JUMP_HEIGHT{100};
+
+  GameState state = ON_GROUND;
+
+  InitWindow(800, 600, "Khukuri");
   SetTargetFPS(60);
 
-  Vector2 position{400, 255};
+  Vector2 position{100, 300};
+  Vector2 fallVelocity{100, 20};
+  Vector2 jumpVelocity{100, 500};
 
+  Rectangle player{position.x, position.y, 50, 100};
+  Rectangle ground{0, 400, 800, 200};
   while (!WindowShouldClose()) {
-    if (IsKeyDown(KEY_RIGHT))
-      position.x += 4;
-    if (IsKeyDown(KEY_LEFT))
-      position.x -= 4;
-    if (IsKeyDown(KEY_UP))
-      position.y -= 4;
-    if (IsKeyDown(KEY_DOWN))
-      position.y += 4;
+    float dt = GetFrameTime();
+    if (IsKeyPressed(KEY_UP) && state != FALLING) {
+      state = JUMPING;
+      ;
+    }
+    if (state == JUMPING) {
+      player.y -= jumpVelocity.y * dt;
+      jumpVelocity.y -= 20;
+      if (jumpVelocity.y == 0) {
+        state = FALLING;
+        jumpVelocity.y = 500;
+      }
+    }
+    if (state == FALLING) {
+      fallVelocity.y += 20;
+      player.y += fallVelocity.y * dt;
+    }
+    if (CheckCollisionRecs(player, ground)) {
+      fallVelocity.y = 20;
+      state = ON_GROUND;
+      player.y = ground.y - player.height;
+    }
+
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawText("Hello, World!", position.x, position.y, 20, LIGHTGRAY);
+    DrawRectangleRec(player, BLUE);
+    DrawRectangleRec(ground, GREEN);
     EndDrawing();
   }
   CloseWindow();
