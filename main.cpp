6a1609627d55ca635d24fc67e7d6bb08f33089fa
Anish-Bikram-Nembang <anish.bikram.nembang@gmail.com>
2026-09-02 13:04:00 +0545

    vertically moving players and a boundary

--- main.cpp
+++ main.cpp
@@ -8,10 +8,13 @@ int main(void) {
 
   enum GameState { PLAYING, GAME_OVER };
   GameState state = GAME_OVER;
-  Vector2 velocity{200, 20};
+  Vector2 velocity{200, 300};
 
-  Rectangle playerA{350, 80, 100, 20};
-  Rectangle playerB{350, 500, 100, 20};
+  Rectangle playerA{40, 250, 20, 100};
+  Rectangle playerB{740, 250, 20, 100};
+
+  Rectangle upperBoundary{0, 0, 800, 20};
+  Rectangle lowerBoundary{0, 580, 800, 20};
 
   Vector2 circlePosition{390, 290};
 
@@ -20,14 +23,16 @@ int main(void) {
 
   while (!WindowShouldClose()) {
     float dt = GetFrameTime();
-    handlePlayerMovement(&playerA, IsKeyDown(KEY_A), IsKeyDown(KEY_D),
-                         velocity.x, dt);
-    handlePlayerMovement(&playerB, IsKeyDown(KEY_LEFT), IsKeyDown(KEY_RIGHT),
-                         velocity.x, dt);
+    handlePlayerMovement(&playerA, IsKeyDown(KEY_W), IsKeyDown(KEY_S),
+                         velocity.y, dt);
+    handlePlayerMovement(&playerB, IsKeyDown(KEY_UP), IsKeyDown(KEY_DOWN),
+                         velocity.y, dt);
     BeginDrawing();
     ClearBackground(RAYWHITE);
     DrawRectangleRec(playerA, BLUE);
     DrawRectangleRec(playerB, GREEN);
+    DrawRectangleRec(upperBoundary, PURPLE);
+    DrawRectangleRec(lowerBoundary, PURPLE);
     DrawCircle(circlePosition.x, circlePosition.y, 10, RED);
     EndDrawing();
   }
@@ -38,17 +43,17 @@ void checkAndUpdateScore() {}
 void handlePlayerMovement(Rectangle *player, bool leftMovement,
                           bool rightMovement, float velocity, float dt) {
   if (leftMovement) {
-    if (!(player->x <= 0))
-      player->x -= velocity * dt;
+    if (!(player->y <= 20))
+      player->y -= velocity * dt;
   }
-  if (player->x <= 0) {
-    player->x = 0;
+  if (player->y <= 20) {
+    player->y = 20;
   }
   if (rightMovement) {
-    if (!(player->x >= 700))
-      player->x += velocity * dt;
+    if (!(player->y >= 480))
+      player->y += velocity * dt;
   }
-  if (player->x >= 700) {
-    player->x = 700;
+  if (player->y >= 480) {
+    player->y = 480;
   }
 }
