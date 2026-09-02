d028e4f410cb51e9773b592b9db631cc1f3c8f98
Anish-Bikram-Nembang <anish.bikram.nembang@gmail.com>
2026-09-02 12:53:45 +0545

    moving pong players

--- main.cpp
+++ main.cpp
@@ -1,34 +1,54 @@
 #include <raylib.h>
-#define GRAVITY 980
-#define JUMP_SPEED 200
 
+void handlePlayerMovement(Rectangle *player, bool leftMovement,
+                          bool rightMovement, float velocity, float dt);
 int main(void) {
-  InitWindow(800, 600, "Khukuri");
+  InitWindow(800, 600, "Pong");
   SetTargetFPS(60);
 
-  Vector2 position{100, 300};
-  Vector2 velocity{100, 20};
+  enum GameState { PLAYING, GAME_OVER };
+  GameState state = GAME_OVER;
+  Vector2 velocity{200, 20};
 
-  Rectangle player{position.x, position.y, 50, 100};
-  Rectangle ground{0, 400, 800, 200};
+  Rectangle playerA{350, 80, 100, 20};
+  Rectangle playerB{350, 500, 100, 20};
+
+  Vector2 circlePosition{390, 290};
+
+  int playerAScore = 0;
+  int playerBScore = 0;
+
   while (!WindowShouldClose()) {
     float dt = GetFrameTime();
-    if (IsKeyPressed(KEY_UP)) {
-      velocity.y = -JUMP_SPEED;
-    }
-    velocity.y += GRAVITY * dt;
-    player.y += velocity.y * dt;
-    if (CheckCollisionRecs(player, ground) && velocity.y > 0) {
-      velocity.y = 0;
-      player.y = ground.y - player.height;
-    }
-
+    handlePlayerMovement(&playerA, IsKeyDown(KEY_A), IsKeyDown(KEY_D),
+                         velocity.x, dt);
+    handlePlayerMovement(&playerB, IsKeyDown(KEY_LEFT), IsKeyDown(KEY_RIGHT),
+                         velocity.x, dt);
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawRectangleRec(player, BLUE);
-    DrawRectangleRec(ground, GREEN);
+    DrawRectangleRec(playerA, BLUE);
+    DrawRectangleRec(playerB, GREEN);
+    DrawCircle(circlePosition.x, circlePosition.y, 10, RED);
     EndDrawing();
   }
   CloseWindow();
   return 0;
+}
+void checkAndUpdateScore() {}
+void handlePlayerMovement(Rectangle *player, bool leftMovement,
+                          bool rightMovement, float velocity, float dt) {
+  if (leftMovement) {
+    if (!(player->x <= 0))
+      player->x -= velocity * dt;
+  }
+  if (player->x <= 0) {
+    player->x = 0;
+  }
+  if (rightMovement) {
+    if (!(player->x >= 700))
+      player->x += velocity * dt;
+  }
+  if (player->x >= 700) {
+    player->x = 700;
+  }
 }
